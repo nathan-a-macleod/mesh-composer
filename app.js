95fbde3d51abebc0e851123047baf9bee33b2834
Jacob and Nathan MacLeod <48350114+jacob-and-nathan@users.noreply.github.com>
2020-04-07 16:19:23 +0100

    Add RGB slider for colour

--- app.js
+++ app.js
@@ -47,6 +47,63 @@ document.getElementById('toggleEditMode').addEventListener('click', ()=> {
   camera.layers.toggle(1);
 });
 
+// The RGB slider:
+var body = document.body, 
+  r = document.querySelector('#redSlider'),
+  g = document.querySelector('#greenSlider'),
+  b = document.querySelector('#blueSlider'),
+  outputR = document.querySelector('#outputR'),
+  outputG = document.querySelector('#outputG'),
+  b_out = document.querySelector('#outputB'),
+  hexVal_out = document.querySelector('#changeColor');
+
+outputR.value = '255';
+outputG.value = '255';
+outputB.value = '255';
+
+function setColor(){
+  var r_hexVal = parseInt(r.value, 10).toString(16),
+    g_hexVal = parseInt(g.value, 10).toString(16),
+    b_hexVal = parseInt(b.value, 10).toString(16),
+    hexVal = "#" + pad(r_hexVal) + pad(g_hexVal) + pad(b_hexVal);
+  BoxMeshName.setColor(hexVal_out.value);
+  hexVal_out.value = hexVal;
+}
+
+function pad(n){
+  return (n.length<2) ? "0"+n : n;
+}
+ 
+r.addEventListener('change', function() {
+  setColor();
+  outputR.value = r.value;
+}, false);
+ 
+r.addEventListener('input', function() {
+  setColor();
+  outputR.value = r.value;
+}, false);
+ 
+g.addEventListener('change', function() {
+  setColor();
+  outputG.value = g.value;
+}, false);
+ 
+g.addEventListener('input', function() {
+  setColor();
+  outputG.value = g.value;
+}, false);
+ 
+b.addEventListener('change', function() {
+  setColor();
+  b_out.value = b.value;
+}, false);
+ 
+b.addEventListener('input', function() {
+  setColor();
+  b_out.value = b.value;
+}, false);
+
 // Define some objects:
 function CreatePlaneGeometry() {
   this.planeMaterialName;
