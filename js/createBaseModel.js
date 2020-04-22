e2f64ef992153b389b89354e2e1f7a9c0c936cee
Jacob and Nathan MacLeod <48350114+jacob-and-nathan@users.noreply.github.com>
2020-04-22 15:54:00 +0100

    Fix some issues

--- js/createBaseModel.js
+++ js/createBaseModel.js
@@ -16,34 +16,36 @@ projectionPlane.layers.set(2);
 scene.add(projectionPlane);
 
 function onMouseClick(event) {
-	// calculate mouse position in normalized device coordinates
-	// (-1 to +1) for both components
-	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-	mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-	
-	ray.layers.set(2);
-	ray.setFromCamera(mouse, camera);
-	intersects = ray.intersectObjects(scene.children);
-	
-	for (var i = 0; i < intersects.length; i++){
-	  // Need to add where you click (intersects[i].point) to an array to remember which
-	  // vertices you are creating, then create lines between them
-  	geometry = new THREE.BoxGeometry(0.05, 0.05, 0.05);
-    material = new THREE.MeshBasicMaterial({color: 0x3f68d9});
-    cube = new THREE.Mesh(geometry, material);
-    cube.position.x = intersects[i].point.x;
-    cube.position.y = intersects[i].point.y;
-    cube.position.z = 0;
-    cube.layers.set(3);
-    scene.add(cube);
-    
-    lineMaterial = new THREE.LineBasicMaterial({color: 0xffffff});
-    points.push(new THREE.Vector3(cube.position.x, cube.position.y, cube.position.z));
-    lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-    line = new THREE.Line(lineGeometry, lineMaterial);
-    line.layers.set(3);
-    scene.add(line);
-	}
+  if(mouseOnMenu == false){
+    // calculate mouse position in normalized device coordinates
+  	// (-1 to +1) for both components
+  	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  	mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+  	
+  	ray.layers.set(2);
+  	ray.setFromCamera(mouse, camera);
+  	intersects = ray.intersectObjects(scene.children);
+  	
+  	for (var i = 0; i < intersects.length; i++){
+  	  // Need to add where you click (intersects[i].point) to an array to remember which
+  	  // vertices you are creating, then create lines between them
+    	geometry = new THREE.BoxGeometry(0.05, 0.05, 0.05);
+      material = new THREE.MeshBasicMaterial({color: 0x3f68d9});
+      cube = new THREE.Mesh(geometry, material);
+      cube.position.x = intersects[i].point.x;
+      cube.position.y = intersects[i].point.y;
+      cube.position.z = 0;
+      cube.layers.set(3);
+      scene.add(cube);
+      
+      lineMaterial = new THREE.LineBasicMaterial({color: 0xffffff});
+      points.push(new THREE.Vector3(cube.position.x, cube.position.y, cube.position.z));
+      lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+      line = new THREE.Line(lineGeometry, lineMaterial);
+      line.layers.set(3);
+      scene.add(line);
+  	}
+  }
 }
 
 document.body.addEventListener('keydown', function(event){
@@ -73,7 +75,7 @@ document.body.addEventListener('keydown', function(event){
       camera.layers.disable(3);
       cameraOrbit = true;
       
-      document.getElementById('menuUnexpanded').style.display = 'block';
+      //document.getElementById('menuUnexpanded').style.display = 'block';
     }
   }
 });
